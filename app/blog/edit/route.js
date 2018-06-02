5f873770f8518f7c666ecccf70754eca9d9070cf
Admin <tao@admins-mbp.attlocal.net>
2018-06-02 16:52:02 -0500

    Added editable toggle button

--- app/blog/edit/route.js
+++ app/blog/edit/route.js
@@ -1,10 +1,22 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+
+const { set } = Ember;
+
 
 export default Route.extend({
+
+  setupController(controller, model) {
+    set(controller, 'post', model);
+    set(controller, 'editable', true);
+  },
 
   actions: {
     goBackToIndex() {
       this.transitionTo('blog.index');
+    },
+    toggleEdit() {
+      this.controller.toggleProperty('editable');
     }
   }
 });
